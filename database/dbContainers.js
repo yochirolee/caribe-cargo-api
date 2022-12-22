118b3ae7ba6d09b92b210f12f786b7e68fd55652
yochirolee <yleecruz@gmail.com>
2022-12-22 12:29:56 -0500

    order by

--- database/dbContainers.js
+++ database/dbContainers.js
@@ -1,7 +1,7 @@
 import { query } from "../services/db.js";
 
 export const db_getContainers = async () => {
-	return await query("SELECT codigo as ContainerId, fecha as StartLoadingDate, fecha_salida  as EndLoadingDate, master as Master, paquetes as ProductsQuantity,peso as Weight, sello as SealedNumber, estado as State FROM contenedores LIMIT 50");
+	return await query("SELECT codigo as ContainerId, fecha as StartLoadingDate, fecha_salida  as EndLoadingDate, master as Master, paquetes as ProductsQuantity,peso as Weight, sello as SealedNumber, estado as State FROM contenedores ORDER BY  codigo Desc");
 };
 
 export const db_getContainerByState = async (containerState) => {
