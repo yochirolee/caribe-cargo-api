13474c04d5179cc53748c902f04f5c2d22f6d10c
yochirolee <yleecruz@gmail.com>
2022-12-22 16:40:12 -0500

    container number addded to query

--- database/dbContainers.js
+++ database/dbContainers.js
@@ -1,16 +1,21 @@
 import { query } from "../services/db.js";
 
 export const db_getContainers = async () => {
-	return await query("SELECT codigo as ContainerId, fecha as StartLoadingDate, fecha_salida  as EndLoadingDate, master as Master, paquetes as ProductsQuantity,peso as Weight, sello as SealedNumber, estado as State FROM contenedores ORDER BY  codigo Desc");
+	return await query(
+		"SELECT codigo as ContainerId, fecha as StartLoadingDate, fecha_salida  as EndLoadingDate, master as Master, paquetes as ProductsQuantity,peso as Weight, sello as SealedNumber, estado as State,numero as ContainerNumber FROM contenedores ORDER BY  codigo Desc",
+	);
 };
 
 export const db_getContainerByState = async (containerState) => {
-	return await query("SELECT codigo as ContainerId, fecha as StartLoadingDate, fecha_salida  as EndLoadingDate, master as Master, paquetes as ProductsQuantity,peso as Weight, sello as SealedNumber, estado as State  from contenedores where estado=?", [containerState]);
+	return await query(
+		"SELECT codigo as ContainerId, fecha as StartLoadingDate, fecha_salida  as EndLoadingDate, master as Master, paquetes as ProductsQuantity,peso as Weight, sello as SealedNumber, estado as State,numero as ContainerNumber  from contenedores where estado=?",
+		[containerState],
+	);
 };
 
 export const db_getContainerById = async (id) => {
 	const result = await query(
-		"SELECT codigo as ContainerId, fecha as StartLoadingDate, fecha_salida  as EndLoadingDate, master as Master, paquetes as ProductsQuantity,peso as Weight, sello as SealedNumber, estado as State  FROM contenedores WHERE codigo=? ",
+		"SELECT codigo as ContainerId, fecha as StartLoadingDate, fecha_salida  as EndLoadingDate, master as Master, paquetes as ProductsQuantity,peso as Weight, sello as SealedNumber, estado as State,numero as ContainerNumber  FROM contenedores WHERE codigo=? ",
 		[id],
 	);
 
