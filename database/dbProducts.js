9201168782bd59779a83244fc0dd343856ad4e97
yochirolee <yleecruz@gmail.com>
2023-01-31 15:35:40 -0500

    tracking select container

--- database/dbProducts.js
+++ database/dbProducts.js
@@ -21,8 +21,5 @@ export const db_findProducts = async (ListHBL) => {
 };
 
 export const db_getProductsByContainerId = async (ContainerId) => {
-	return await query(
-		"SELECT codigo_paquete as HBL,orden_envio.cod_envio as InvoiceId, descripcion as Description,orden_envio.cliente as CustomerId,orden_envio.destinatario as RecieverId , estado as Location,num_contenedor as ContainerNumber, pallet as Pallet, agencias.nombre as Agency FROM orden_envio_emp_det INNER JOIN orden_envio ON orden_envio_emp_det.cod_envio=orden_envio.cod_envio INNER JOIN agencias ON orden_envio_emp_det.agencia=agencias.id WHERE contenedor=?",
-		[ContainerId],
-	);
+	return await query("SELECT * FROM tracking WHERE ContainerId=?", [ContainerId]);
 };
